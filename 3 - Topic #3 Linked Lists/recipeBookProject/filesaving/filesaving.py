0ef87498e3afb254c90692ae810459d944ecd9c7
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2023-11-12 21:56:47 +0000

    Finalize the LinkedList Project

--- 3 - Topic #3 Linked Lists/recipeBookProject/filesaving/filesaving.py
+++ 3 - Topic #3 Linked Lists/recipeBookProject/filesaving/filesaving.py
@@ -1,6 +1,5 @@
 """Saves a List to a binary file"""
 import pickle
-
 class Savedata():
     """Saves data to and from a file
     """
@@ -11,8 +10,14 @@ class Savedata():
             fileName (str): the name of the file to save to
             data (obj): the data to save to the file
         """
-        with open(fileName, 'wb') as f:
-            pickle.dumps(data, f)
+        try:
+            with open(fileName, 'wb') as f:
+                pickle.dump(data, f)
+        except FileNotFoundError:
+            # create the file, then save
+            open(fileName, 'x')
+            with open(fileName, 'wb') as f:
+                pickle.dump(data, f)
             
     def load_file(self, fileName = "savedata.bin"):
         """Load the data from the file
@@ -24,4 +29,4 @@ class Savedata():
             obj: the data loaded from the file
         """
         with open(fileName, 'rb') as f:
-            return pickle.loads(f)
+            return pickle.load(f)
