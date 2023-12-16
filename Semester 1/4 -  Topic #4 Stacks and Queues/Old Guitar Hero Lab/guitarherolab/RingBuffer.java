2e398738736227d327c26bc0bec7e111c4517f12
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2023-12-16 17:37:26 +0000

    Heavy Refactoring (Again)

---
+++ Semester 1/4 -  Topic #4 Stacks and Queues/Old Guitar Hero Lab/guitarherolab/RingBuffer.java
@@ -0,0 +1,73 @@
+import java.util.NoSuchElementException;
+
+public class RingBuffer {
+
+    private int capacity;
+    private int size;
+    private int first;
+    private int last;
+    private double[] buffer;
+
+    public RingBuffer(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.buffer = new double[capacity];
+        this.first = 0;
+        this.last = 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+    public int capacity() {
+        return this.capacity;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public boolean isFull() {
+        return this.size == this.capacity;
+    }
+
+    public void enqueue(double x) {
+        if (isFull()) {
+            throw new IllegalStateException("Cannot enqueue to a full buffer.");
+        }
+        buffer[last] = x;
+        last = (last + 1) % capacity; // Circularly link the buffer
+        size++;
+    }
+
+    public double dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot dequeue from an empty buffer.");
+        }
+        double item = buffer[first];
+        first = (first + 1) % capacity; // Circularly link the buffer
+        size--;
+        return item;
+    }
+
+    public double peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Cannot peek an empty buffer.");
+        }
+        return buffer[first];
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < size; i++) {
+            int index = (first + i) % capacity;
+            sb.append(buffer[index]);
+            if (i < size - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
